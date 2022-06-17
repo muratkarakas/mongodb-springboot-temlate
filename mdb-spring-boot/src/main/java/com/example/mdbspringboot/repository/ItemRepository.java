75387ab8a0974e1e109a9231521c3361ac118e09
Murat Karakas <murat.karakas@eteration.com>
2022-06-17 11:56:19 +0300

    clean up

--- mdb-spring-boot/src/main/java/com/example/mdbspringboot/repository/ItemRepository.java
+++ mdb-spring-boot/src/main/java/com/example/mdbspringboot/repository/ItemRepository.java
@@ -12,7 +12,7 @@ public interface ItemRepository extends MongoRepository<GroceryItem, String> {
 	@Query("{name:'?0'}")
 	GroceryItem findItemByName(String name);
 	
-	@Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+	@Query(value="{category:'?0'}")
 	List<GroceryItem> findAll(String category);
 	
 	public long count();
